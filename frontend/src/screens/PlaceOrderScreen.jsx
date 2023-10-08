a8ffc674112266f0e2a3cd504d6d70a1c7c6a89e
Abdulrahman Olaobaju <olaobajurahmon@gmail.com>
2023-10-08 21:57:14 +0300

    Order endpoints and frontend consumption

---
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -0,0 +1,147 @@
+import { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
+import CheckoutSteps from '../components/CheckoutSteps'
+import { toast } from 'react-toastify'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { useCreateOrderMutation } from '../slices/ordersApiSlice'
+import { clearCartItems } from '../slices/cartSlice'
+
+const PlaceOrderScreen = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const cart = useSelector((state) => state.cart)
+
+  const [createOrder, { isLoading, error }] = useCreateOrderMutation()
+  useEffect(() => {
+    if (!cart.shippingAddress.address) {
+      navigate('/shipping')
+    } else if (!cart.paymentMethod) {
+      navigate('/payment')
+    }
+  }, [navigate, cart.paymentMethod, cart.shippingAddress.address])
+
+  const placeOrderHandler = async () => {
+    try {
+      const res = await createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        taxPrice: cart.taxPrice,
+        shippingPrice: cart.shippingPrice,
+        totalPrice: cart.totalPrice,
+      }).unwrap()
+      dispatch(clearCartItems())
+      navigate(`/order/${res._id}`)
+    } catch (err) {
+      toast.error(err)
+    }
+  }
+  return (
+    <>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Address: </strong>
+                {cart.shippingAddress.address},{' '}
+                {cart.shippingAddress.postalCode} {cart.shippingAddress.city},{' '}
+                {cart.shippingAddress.country}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <p>
+                <strong>Method: </strong>
+                {cart.paymentMethod}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Items in Order</h2>
+              {cart.cartItems.length === 0 ? (
+                <Message>Your cart is empty</Message>
+              ) : (
+                <ListGroup variant='flush'>
+                  {cart.cartItems.map((item) => (
+                    <ListGroup.Item key={item._id}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/product/${item._id}`}>{item.name}</Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} x €{item.price} = €{item.qty * item.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items:</Col>
+                  <Col>€{cart.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping:</Col>
+                  <Col>€{cart.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax:</Col>
+                  <Col>€{cart.taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total:</Col>
+                  <Col>€{cart.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                {error && <Message variant='danger'>{error}</Message>}
+              </ListGroup.Item>
+              <ListGroup.Item className='d-grid gap-2'>
+                <Button
+                  type='button'
+                  disabled={cart.cartItems.length === 0}
+                  onClick={placeOrderHandler}
+                >
+                  Place Order
+                </Button>
+                {isLoading && <Loader />}
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default PlaceOrderScreen
