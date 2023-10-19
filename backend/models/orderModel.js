9adbfea7f72d4b756c95cd1a546dcec929c3106c
Abdulrahman Olaobaju <olaobajurahmon@gmail.com>
2023-10-19 09:26:46 +0300

    Order page and payment route

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -7,7 +7,7 @@ const orderSchema = new mongoose.Schema(
       required: true,
       ref: 'User',
     },
-    OrderItems: [
+    orderItems: [
       {
         name: { type: String, required: true },
         qty: { type: Number, required: true },
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -15,6 +15,6 @@ router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
 router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
 router.route('/myorders').get(protect, getMyOrders)
-router.route('/:id').get(protect, admin, getOrderById)
+router.route('/:id').get(protect, getOrderById)
 
 export default router
