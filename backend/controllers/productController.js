929a71b6915e4205ca54da13e163bbcb58ae41ea
Abdulrahman Olaobaju <olaobajurahmon@gmail.com>
2024-04-04 08:55:58 +0300

    Top rated product carousel on homepage

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -130,6 +130,19 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Fetch 3 top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+  if (products) {
+    return res.status(200).json(products)
+  } else {
+    res.status(404)
+    throw new Error('Resource not found')
+  }
+})
+
 export {
   getProducts,
   getProductById,
@@ -137,4 +150,5 @@ export {
   updateProduct,
   deleteProduct,
   createProductReview,
+  getTopProducts,
 }
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,6 +3,7 @@ import Product from '../components/Product'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 import Paginate from '../components/Paginate'
+import ProductCarousel from '../components/ProductCarousel'
 import { useParams, Link } from 'react-router-dom'
 import { useGetProductsQuery } from '../slices/productsApiSlice'
 
@@ -16,7 +17,9 @@ const HomeScreen = () => {
 
   return (
     <>
-      {keyword && (
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
         <Link to='/' className='btn btn-light mb-4'>
           Go Back
         </Link>
